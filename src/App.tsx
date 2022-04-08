7a45531c6317bb9dae34a5ce61e0a98dbc07dc9d
Piotr-Zacharski <zacharski.p.h@gmail.com>
2022-04-08 11:35:56 +0200

    Added background and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { v4 as uuid4 } from 'uuid'
 import './App.css'
 
 import Header from './components/Header'
+import { Button, Paper } from '@mui/material'
 
-function App() {
+const App:React.FC =() => {
+
+  const [ships, setShips] = useState<any[]>([]);
+  const [areShips, setAreShips] = useState(false);
+
+const fetchShips = () => {
+  axios
+    .get(`https://api.spacexdata.com/v3/ships`)
+    .then((res) => {
+      console.log(res);
+      setShips(res.data.slice(0,4));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const hideShips = () => {
+  setShips([]);
+  setAreShips(false);
+}
+
 
   return (
     <div className="App">
       <Header />
+      {!areShips && <Button style={{color: 'white'}} onClick={() => fetchShips()}>Fetch</Button>}
+      {areShips && <Button onClick={() => hideShips()}>Hide</Button>}
+      {ships.map((ship) => {
+      return(
+          <Paper key={uuid4()} elevation={24} style={{width: 400, textAlign: 'center', margin: '0 auto', objectFit: 'cover'}}>
+            <h4>Nazwa: {ship.ship_name}</h4>
+            <h5>Typ: {ship.ship_type}</h5>
+            <h5>Rola: {ship.roles}</h5>
+            <img style={{ width: 300, height: 200}} src={ship.image} alt={ship.ship_name} />
+          </Paper>
+        )})}
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const StyledLink = styled.a`
+color: white;
+font-size: 1.2em;
+padding: 10px 20px;
+text-decoration: none;
+align-items: center;
+text-align: center;
+justify-content: center;
+&:hover {
+    opacity: 50%;
+  }
+`;
 
 const Header:React.FC = () => {
 
@@ -11,7 +26,7 @@ const Header:React.FC = () => {
           ['Projects', '/projects'],
           ['Reports', '/reports'],
         ].map(([title, url]) => (
-          <a href={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900 text-white">{title}</a>
+          <StyledLink href={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900 text-white">{title}</StyledLink>
           ))}
     </nav>
     </>
